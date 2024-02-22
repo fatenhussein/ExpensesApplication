5396569accb2aa39994d58fc6f30898f2471f115
faten282 <112074192+fatenhussein@users.noreply.github.com>
2024-02-23 00:02:39 +0300

    faten huseein   2/22/2024 12:02am

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// Middleware to set CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+
+module.exports = app;
