1f572f3970700e4d0fa306e32ea6a51a0c880a77
faten282 <112074192+fatenhussein@users.noreply.github.com>
2024-02-23 02:15:22 +0300

    faten huseein  expenses

--- controllers/expensesController.js
+++ controllers/expensesController.js
@@ -56,10 +56,37 @@ exports.getExpense = async (req, res) => {
 
 //Update expense:
 exports.updateExpense = async (req, res) => {
-
+  try {
+    //By default, findOneAndUpdate() returns the document as it was before update was applied.
+    //If you set new: true, findOneAndUpdate() will instead give you the object after update was applied.
+    const expense = await Expense.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      expense,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
 
 //Delete expense:
 exports.deleteExpense = async (req, res) => {
- 
+  try {
+    const expense = await Expense.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      expense,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
