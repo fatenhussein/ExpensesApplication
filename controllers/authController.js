d39d25a5b534b775ae83b00f6825c193883e2fbb
faten282 <112074192+fatenhussein@users.noreply.github.com>
2024-02-23 00:19:41 +0300

    faten huseein   2/22/2024 12:19am

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,25 +1,25 @@
 const User = require('../models/usersModel');
 
 //Create user:
-exports.createuser = async (req, res) => ({
-
-
-
-  
-});
+exports.createuser = async (req, res) => {
+  try {
+    const newuser = await User.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user: newuser,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
 
 //login user:
-exports.login = async (req, res) => ({
-
-
-
-});
-
+exports.login = async (req, res) => ({});
 
 //logout
-exports.logout = async (req, res) => ({
-
-
-
-
-});
+exports.logout = async (req, res) => ({});
