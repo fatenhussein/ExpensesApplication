128efbd7c8f074a50c3b68d49d9e40c2d74104b9
faten282 <112074192+fatenhussein@users.noreply.github.com>
2024-02-23 14:08:45 +0300

    faten huseein  swagger for the users api

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,3 +7,75 @@ router.route('/signIn').post(login);
 router.route('/logout').post(logout);
 
 module.exports = router;
+
+
+/**
+ * @swagger
+ * /users/signIn:
+ *   post:
+ *     summary: Log in a user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - email
+ *               - password
+ *     responses:
+ *       '200':
+ *         description: User logged in successfully
+ *       '401':
+ *         description: Unauthorized, invalid credentials
+ */
+
+/**
+ * @swagger
+ * /users/logout:
+ *   post:
+ *     summary: Log out a user
+ *     tags: [Authentication]
+ *     responses:
+ *       '200':
+ *         description: User logged out successfully
+ *       '401':
+ *         description: Unauthorized, user not logged in
+ */
+
+
+/**
+ * @swagger
+ * /users/signUp:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Authentication]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             required:
+ *               - userName
+ *               - email
+ *               - password
+ *     responses:
+ *       '200':
+ *         description: User registered successfully
+ *       '400':
+ *         description: Invalid request payload
+ */
--- utils/swagger.js
+++ utils/swagger.js
@@ -17,6 +17,47 @@ const options = {
         description: 'Development server',
       },
     ],
+    components: {
+      schemas: {
+        User: {
+          type: 'object',
+          properties: {
+            userName: {
+              type: 'string',
+              description: 'The username.',
+            },
+            email: {
+              type: 'string',
+              description: 'The email address.',
+            },
+            password: {
+              type: 'string',
+              description: "The user's password.",
+            },
+            createdAt: {
+              type: 'string',
+              format: 'date-time',
+              description: 'The date and time when the user was created.',
+            },
+            updatedAt: {
+              type: 'string',
+              format: 'date-time',
+              description: 'The date and time when the user was last updated.',
+            },
+            expenses: {
+              type: 'array',
+              items: {
+                type: 'string',
+                description: 'The ID of an expense associated with the user.',
+              },
+              description: 'An array of expense IDs associated with the user.',
+            },
+          },
+          required: ['userName', 'email', 'password'],
+        },
+      },
+    },
+    
   },
   // Path to the APIs you want to document
   apis: ['./routes/*.js'], // Path to the API routes
