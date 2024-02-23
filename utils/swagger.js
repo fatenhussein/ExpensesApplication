737b476c223b1b2f9e33eccd9e357dfc4881a7bb
faten282 <112074192+fatenhussein@users.noreply.github.com>
2024-02-23 11:42:02 +0300

    faten huseein  swagger

--- utils/swagger.js
+++ utils/swagger.js
@@ -1,2 +1,33 @@
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+
+// Swagger options
+const options = {
+  // Define the Swagger definition
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'ExpensesApp API',
+      version: '1.0.0',
+      description: 'API documentation for ExpensesApp',
+    },
+    servers: [
+      {
+        url: 'http://localhost:9000',
+        description: 'Development server',
+      },
+    ],
+  },
+  // Path to the APIs you want to document
+  apis: ['./routes/*.js'], // Path to the API routes
+};
+
+// Initialize Swagger-jsdoc
+const swaggerSpec = swaggerJSDoc(options);
+
+// Set up Swagger UI
+const setupSwagger = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
+
+module.exports = setupSwagger;
